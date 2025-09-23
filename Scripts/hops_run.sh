108d9836444021f9496ee8aa69cc7be8ad1d9095
DawnALewis <101039766+DawnALewis@users.noreply.github.com>
2025-09-23 21:56:02 +0930

    Update hops_run.sh

--- Scripts/hops_run.sh
+++ Scripts/hops_run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#SBATCH -p batch
+#SBATCH -N 1
+#SBATCH --time=72:00:00
+#SBATCH --mem=199GB
+#SBATCH -c 32
 
 ml Singularity
-singularity exec -B /gpfs/ /hpcfs/groups/acad_users/containers/nf-core-eager_2.4.5-sharding.sif hops \
-  -m me_po \
-  -i /gpfs/users/a1867445/Widgingarri/malt_conda/*.rma6 \
-  -o /gpfs/users/a1867445/Widgingarri/malt_conda/hops/
+singularity exec -B /gpfs/ /hpcfs/groups/acad_users/shyrav/resources/singularity/nf-core-eager_2.4.5-sharding.sif hops -i *.rma6 -output HOPS -m me_po -c HOPS_configFile_Melioidosis.txt
